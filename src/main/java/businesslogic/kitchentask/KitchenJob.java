2fcc037fdf0e345f531ce0c530571820ddba2e9a
Stefa168 <stefa168@hotmail.it>
2020-06-29 01:25:24 +0200

    Implementati metodi toString() mancanti

--- src/main/java/businesslogic/kitchentask/KitchenJob.java
+++ src/main/java/businesslogic/kitchentask/KitchenJob.java
@@ -23,8 +23,45 @@ public class KitchenJob implements TaskItemInfo {
         this.estimatedDuration = estimatedDuration;
     }
 
-    public KitchenJob (){
+    public KitchenJob() {
 
+    }
+
+    public static ArrayList<KitchenJob> getAllKitchenJobs() {
+        String query = "SELECT * FROM KitchenJob WHERE " + true;
+        ArrayList<KitchenJob> kitchenJobs = new ArrayList<>();
+
+        PersistenceManager.executeQuery(query, new ResultHandler() {
+            @Override
+            public void handle(ResultSet rs) throws SQLException {
+                KitchenJob kitchenJob = new KitchenJob();
+                kitchenJob.amount = rs.getInt("amount");
+                int duration = rs.getInt("estimatedDuration");
+                kitchenJob.estimatedDuration = Duration.ofMinutes(duration);
+                kitchenJob.cook = User.loadUserById(rs.getInt("cook_id"));
+                kitchenJob.turn = KitchenTurn.loadKitchenTurnById(rs.getInt("turn_id"));
+                kitchenJobs.add(kitchenJob);
+            }
+        });
+        return kitchenJobs;
+    }
+
+    public static KitchenJob getKitchenJobById(int task_id, int cook_id, int turn_id) {
+        String query = "SELECT * FROM KitchenJob WHERE task_id = " + task_id + "AND cook_id = " + cook_id + "AND " +
+                       "turn_id = " + turn_id;
+        KitchenJob kitchenJob = new KitchenJob();
+
+        PersistenceManager.executeQuery(query, new ResultHandler() {
+            @Override
+            public void handle(ResultSet rs) throws SQLException {
+                kitchenJob.amount = rs.getInt("amount");
+                int duration = rs.getInt("estimatedDuration");
+                kitchenJob.estimatedDuration = Duration.ofMinutes(duration);
+                kitchenJob.cook = User.loadUserById(rs.getInt("cook_id"));
+                kitchenJob.turn = KitchenTurn.loadKitchenTurnById(rs.getInt("turn_id"));
+            }
+        });
+        return kitchenJob;
     }
 
     public void edit(int amount, Duration estimatedDuration) {
@@ -49,6 +86,12 @@ public class KitchenJob implements TaskItemInfo {
         return estimatedDuration;
     }
 
+    public void setDuration(Duration newDuration) {
+        this.estimatedDuration = newDuration;
+    }
+
+    //metodi per il db
+
     public KitchenTurn getTurn() {
         return turn;
     }
@@ -57,45 +100,19 @@ public class KitchenJob implements TaskItemInfo {
         return cook;
     }
 
-    public void setDuration(Duration newDuration) {
-        this.estimatedDuration = newDuration;
-    }
-
-    //metodi per il db
-
-    public static ArrayList<KitchenJob> getAllKitchenJobs(){
-        String query = "SELECT * FROM KitchenJob WHERE " + true;
-        ArrayList<KitchenJob> kitchenJobs = new ArrayList<>();
-
-        PersistenceManager.executeQuery(query, new ResultHandler() {
-            @Override
-            public void handle(ResultSet rs) throws SQLException {
-                KitchenJob kitchenJob = new KitchenJob();
-                kitchenJob.amount = rs.getInt("amount");
-                int duration = rs.getInt("estimatedDuration");
-                kitchenJob.estimatedDuration = Duration.ofMinutes(duration);
-                kitchenJob.cook = User.loadUserById(rs.getInt("cook_id"));
-                kitchenJob.turn = KitchenTurn.loadKitchenTurnById(rs.getInt("turn_id"));
-                kitchenJobs.add(kitchenJob);
-            }
-        });
-        return kitchenJobs;
-    }
-
-    public static KitchenJob getKitchenJobById(int task_id, int cook_id, int turn_id){
-        String query = "SELECT * FROM KitchenJob WHERE task_id = " + task_id + "AND cook_id = " + cook_id + "AND turn_id = " + turn_id;
-        KitchenJob kitchenJob = new KitchenJob();
-
-        PersistenceManager.executeQuery(query, new ResultHandler() {
-            @Override
-            public void handle(ResultSet rs) throws SQLException {
-                kitchenJob.amount = rs.getInt("amount");
-                int duration = rs.getInt("estimatedDuration");
-                kitchenJob.estimatedDuration = Duration.ofMinutes(duration);
-                kitchenJob.cook = User.loadUserById(rs.getInt("cook_id"));
-                kitchenJob.turn = KitchenTurn.loadKitchenTurnById(rs.getInt("turn_id"));
-            }
-        });
-        return kitchenJob;
+    @Override
+    public String toString() {
+        return cook != null ?
+                String.format("Incarico per %d porzioni con una durata di %d minuti.\nAssegnato nel turno %s\nCuoco " +
+                              "scelto: %s.",
+                              amount,
+                              estimatedDuration.toMinutes(),
+                              turn.toString(),
+                              cook.toString()) :
+                String.format("Incarico per %d porzioni con una durata di %d minuti.\nAssegnato nel turno %s\nNessun " +
+                              "cuoco scelto.",
+                              amount,
+                              estimatedDuration.toMinutes(),
+                              turn.toString());
     }
 }
