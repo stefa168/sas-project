05abe72ee351d12c879bdda57b8c28b682f89ab1
Stefa168 <stefa168@hotmail.it>
2020-06-28 18:23:38 +0200

    Implementate alcune funzionalità del passo 1

--- src/main/java/ui/task/TaskManagement.java
+++ src/main/java/ui/task/TaskManagement.java
@@ -4,9 +4,11 @@ import businesslogic.CatERing;
 import businesslogic.event.Event;
 import businesslogic.event.EventItemInfo;
 import businesslogic.event.Service;
+import businesslogic.user.User;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
@@ -27,19 +29,30 @@ public class TaskManagement {
     }
 
     public void initialize() {
-        eventServiceTree.setOnMouseClicked(event -> {
-            if (getSelectedTreeItem() instanceof Service) {
-                apriButton.setDisable(false);
-            } else {
-                apriButton.setDisable(true);
-            }
-        });
+        eventServiceTree.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        eventServiceTree.getSelectionModel().selectedItemProperty().addListener(
+                (observableValue, oldSelection, newSelection) -> {
+                    User u = CatERing.getInstance().getUserManager().getCurrentUser();
+
+                    EventItemInfo selection = newSelection.getValue();
+
+                    apriButton.setDisable(selection == null ||
+                                          selection instanceof Event ||
+                                          ((Service) selection).getParentEvent().getAssignedChef() != u ||
+                                          ((Service)selection).getState() != Service.State.CONFERMATO);
+                });
     }
 
     private EventItemInfo getSelectedTreeItem() {
         return eventServiceTree.getSelectionModel()
                                .getSelectedItem()
                                .getValue();
+    }
+
+    public void clickSuBottoneApri() {
+        // try {
+        //
+        // }
     }
 
     public void startMenuManagement() {
